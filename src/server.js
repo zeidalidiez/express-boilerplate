a51d9158ccba9bd70ad18c3532ae79b215e511d8
zeidalidiez <69106534+zeidalidiez@users.noreply.github.com>
2020-11-11 02:21:55 -0800

    init

---
+++ src/server.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-console */
+/* eslint-disable strict */
+const app = require("./app");
+
+const { PORT } = require("./config");
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
